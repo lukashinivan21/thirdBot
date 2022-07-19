9cb4aa017e415840e2f1d9445246af35d84dd4c6
Ivan <uvanes21@yandex.ru>
2022-07-19 08:48:20 +0300

    Initial commit

---
+++ src/main/java/tgbots/thirdbot/ThirdBotApplication.java
@@ -0,0 +1,13 @@
+package tgbots.thirdbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdBotApplication.class, args);
+    }
+
+}
